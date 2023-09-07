411aeab1bf2247ecdd3c13165b85648825dc736d
Sanderk35 <sanderkamphuis719@gmail.com>
2023-09-07 12:27:00 +0200

    Voorraad systeem

--- Gimpies Winforms/Form1.Designer.cs
+++ Gimpies Winforms/Form1.Designer.cs
@@ -29,6 +29,24 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.Windows.Forms.ListViewItem listViewItem7 = new System.Windows.Forms.ListViewItem(new string[] {
+            "Addidas Daily 3.0",
+            "€64,99",
+            "32 paar",
+            "48",
+            "Zwart"}, -1);
+            System.Windows.Forms.ListViewItem listViewItem8 = new System.Windows.Forms.ListViewItem(new string[] {
+            "Nike Air Max",
+            "€89,99",
+            "15 paar",
+            "48",
+            "Wit"}, -1);
+            System.Windows.Forms.ListViewItem listViewItem9 = new System.Windows.Forms.ListViewItem(new string[] {
+            "Puma Shuffle",
+            "€64,99",
+            "23 paar",
+            "48",
+            "Zwart"}, -1);
             this.UsernameInput = new System.Windows.Forms.TextBox();
             this.PasswordInput = new System.Windows.Forms.TextBox();
             this.Logo = new System.Windows.Forms.PictureBox();
@@ -42,14 +60,23 @@
             this.toevoegenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.aanpassenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.verwijderenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.afsluitenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.Login = new System.Windows.Forms.Panel();
+            this.Storage = new System.Windows.Forms.ListView();
+            this.name = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.price = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.opslag = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.size = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.color = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.Incorrect = new System.Windows.Forms.Panel();
             this.IncorrectText = new System.Windows.Forms.Label();
+            this.sellButton = new System.Windows.Forms.Button();
+            this.paarText = new System.Windows.Forms.Label();
+            this.count = new System.Windows.Forms.NumericUpDown();
             ((System.ComponentModel.ISupportInitialize)(this.Logo)).BeginInit();
             this.menuStrip1.SuspendLayout();
             this.Login.SuspendLayout();
             this.Incorrect.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.count)).BeginInit();
             this.SuspendLayout();
             // 
             // UsernameInput
@@ -124,55 +151,57 @@
             this.verkopenToolStripMenuItem,
             this.toevoegenToolStripMenuItem,
             this.aanpassenToolStripMenuItem,
-            this.verwijderenToolStripMenuItem,
-            this.afsluitenToolStripMenuItem});
+            this.verwijderenToolStripMenuItem});
             this.schoenenToolStripMenuItem.Name = "schoenenToolStripMenuItem";
-            this.schoenenToolStripMenuItem.Size = new System.Drawing.Size(60, 26);
+            this.schoenenToolStripMenuItem.Size = new System.Drawing.Size(60, 24);
             this.schoenenToolStripMenuItem.Text = "Menu";
+            this.schoenenToolStripMenuItem.ToolTipText = "Menu met pagina\'s";
             // 
             // voorraadBekijkenToolStripMenuItem
             // 
             this.voorraadBekijkenToolStripMenuItem.Enabled = false;
             this.voorraadBekijkenToolStripMenuItem.Name = "voorraadBekijkenToolStripMenuItem";
-            this.voorraadBekijkenToolStripMenuItem.Size = new System.Drawing.Size(212, 26);
+            this.voorraadBekijkenToolStripMenuItem.ShortcutKeys = System.Windows.Forms.Keys.F1;
+            this.voorraadBekijkenToolStripMenuItem.Size = new System.Drawing.Size(236, 26);
             this.voorraadBekijkenToolStripMenuItem.Text = "Voorraad bekijken";
+            this.voorraadBekijkenToolStripMenuItem.CheckedChanged += new System.EventHandler(this.voorraadBekijkenToolStripMenuItem_CheckedChanged);
+            this.voorraadBekijkenToolStripMenuItem.Click += new System.EventHandler(this.voorraadBekijkenToolStripMenuItem_Click);
             // 
             // verkopenToolStripMenuItem
             // 
             this.verkopenToolStripMenuItem.Enabled = false;
             this.verkopenToolStripMenuItem.Name = "verkopenToolStripMenuItem";
-            this.verkopenToolStripMenuItem.Size = new System.Drawing.Size(212, 26);
+            this.verkopenToolStripMenuItem.ShortcutKeys = System.Windows.Forms.Keys.F2;
+            this.verkopenToolStripMenuItem.Size = new System.Drawing.Size(236, 26);
             this.verkopenToolStripMenuItem.Text = "Verkopen";
+            this.verkopenToolStripMenuItem.Click += new System.EventHandler(this.verkopenToolStripMenuItem_Click);
             // 
             // toevoegenToolStripMenuItem
             // 
             this.toevoegenToolStripMenuItem.Enabled = false;
             this.toevoegenToolStripMenuItem.Name = "toevoegenToolStripMenuItem";
-            this.toevoegenToolStripMenuItem.Size = new System.Drawing.Size(212, 26);
+            this.toevoegenToolStripMenuItem.ShortcutKeys = System.Windows.Forms.Keys.F3;
+            this.toevoegenToolStripMenuItem.Size = new System.Drawing.Size(236, 26);
             this.toevoegenToolStripMenuItem.Text = "Toevoegen";
+            this.toevoegenToolStripMenuItem.Click += new System.EventHandler(this.toevoegenToolStripMenuItem_Click);
             // 
             // aanpassenToolStripMenuItem
             // 
             this.aanpassenToolStripMenuItem.Enabled = false;
             this.aanpassenToolStripMenuItem.Name = "aanpassenToolStripMenuItem";
-            this.aanpassenToolStripMenuItem.Size = new System.Drawing.Size(212, 26);
+            this.aanpassenToolStripMenuItem.ShortcutKeys = System.Windows.Forms.Keys.F4;
+            this.aanpassenToolStripMenuItem.Size = new System.Drawing.Size(236, 26);
             this.aanpassenToolStripMenuItem.Text = "Aanpassen";
+            this.aanpassenToolStripMenuItem.Click += new System.EventHandler(this.aanpassenToolStripMenuItem_Click);
             // 
             // verwijderenToolStripMenuItem
             // 
             this.verwijderenToolStripMenuItem.Enabled = false;
             this.verwijderenToolStripMenuItem.Name = "verwijderenToolStripMenuItem";
-            this.verwijderenToolStripMenuItem.Size = new System.Drawing.Size(212, 26);
+            this.verwijderenToolStripMenuItem.ShortcutKeys = System.Windows.Forms.Keys.F5;
+            this.verwijderenToolStripMenuItem.Size = new System.Drawing.Size(236, 26);
             this.verwijderenToolStripMenuItem.Text = "Verwijderen";
-            // 
-            // afsluitenToolStripMenuItem
-            // 
-            this.afsluitenToolStripMenuItem.Name = "afsluitenToolStripMenuItem";
-            this.afsluitenToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F4)));
-            this.afsluitenToolStripMenuItem.Size = new System.Drawing.Size(212, 26);
-            this.afsluitenToolStripMenuItem.Text = "Afsluiten";
-            this.afsluitenToolStripMenuItem.ToolTipText = "Alt + F4";
-            this.afsluitenToolStripMenuItem.Click += new System.EventHandler(this.afsluitenToolStripMenuItem_Click);
+            this.verwijderenToolStripMenuItem.Click += new System.EventHandler(this.verwijderenToolStripMenuItem_Click);
             // 
             // Login
             // 
@@ -187,6 +216,52 @@
             this.Login.Name = "Login";
             this.Login.Size = new System.Drawing.Size(322, 406);
             this.Login.TabIndex = 8;
+            // 
+            // Storage
+            // 
+            this.Storage.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.name,
+            this.price,
+            this.opslag,
+            this.size,
+            this.color});
+            this.Storage.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.Storage.HideSelection = false;
+            this.Storage.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem7,
+            listViewItem8,
+            listViewItem9});
+            this.Storage.Location = new System.Drawing.Point(0, 30);
+            this.Storage.MinimumSize = new System.Drawing.Size(254, 4);
+            this.Storage.MultiSelect = false;
+            this.Storage.Name = "Storage";
+            this.Storage.Size = new System.Drawing.Size(567, 541);
+            this.Storage.TabIndex = 10;
+            this.Storage.UseCompatibleStateImageBehavior = false;
+            this.Storage.View = System.Windows.Forms.View.Details;
+            this.Storage.Visible = false;
+            // 
+            // name
+            // 
+            this.name.Text = "Naam";
+            this.name.Width = 120;
+            // 
+            // price
+            // 
+            this.price.Text = "Prijs";
+            // 
+            // opslag
+            // 
+            this.opslag.Text = "Voorraad";
+            this.opslag.Width = 70;
+            // 
+            // size
+            // 
+            this.size.Text = "Maat";
+            // 
+            // color
+            // 
+            this.color.Text = "Kleur";
             // 
             // Incorrect
             // 
@@ -211,6 +286,35 @@
             this.IncorrectText.Text = "De gebruikersnaam of\r\nhet wachtwoord is onjuist\r\nJe hebt nog   pogingen over";
             this.IncorrectText.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
+            // sellButton
+            // 
+            this.sellButton.Location = new System.Drawing.Point(573, 79);
+            this.sellButton.Name = "sellButton";
+            this.sellButton.Size = new System.Drawing.Size(191, 44);
+            this.sellButton.TabIndex = 11;
+            this.sellButton.Text = "Verkoop geselecteerde schoen";
+            this.sellButton.UseVisualStyleBackColor = true;
+            this.sellButton.Visible = false;
+            this.sellButton.Click += new System.EventHandler(this.sellButton_Click);
+            // 
+            // paarText
+            // 
+            this.paarText.AutoSize = true;
+            this.paarText.Location = new System.Drawing.Point(714, 49);
+            this.paarText.Name = "paarText";
+            this.paarText.Size = new System.Drawing.Size(35, 16);
+            this.paarText.TabIndex = 13;
+            this.paarText.Text = "paar";
+            this.paarText.Visible = false;
+            // 
+            // count
+            // 
+            this.count.Location = new System.Drawing.Point(574, 49);
+            this.count.Name = "count";
+            this.count.Size = new System.Drawing.Size(134, 22);
+            this.count.TabIndex = 14;
+            this.count.Visible = false;
+            // 
             // Form1
             // 
             this.AcceptButton = this.LoginButton;
@@ -218,8 +322,11 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.ClientSize = new System.Drawing.Size(800, 511);
-            this.ControlBox = false;
+            this.ClientSize = new System.Drawing.Size(776, 583);
+            this.Controls.Add(this.count);
+            this.Controls.Add(this.paarText);
+            this.Controls.Add(this.sellButton);
+            this.Controls.Add(this.Storage);
             this.Controls.Add(this.Incorrect);
             this.Controls.Add(this.menuStrip1);
             this.Controls.Add(this.Login);
@@ -227,6 +334,7 @@
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuStrip1;
             this.MaximizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(300, 0);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Inloggen";
@@ -237,6 +345,7 @@
             this.Login.PerformLayout();
             this.Incorrect.ResumeLayout(false);
             this.Incorrect.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.count)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -258,9 +367,17 @@
         private System.Windows.Forms.ToolStripMenuItem aanpassenToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem verwijderenToolStripMenuItem;
         private System.Windows.Forms.Panel Login;
-        private System.Windows.Forms.ToolStripMenuItem afsluitenToolStripMenuItem;
         private System.Windows.Forms.Panel Incorrect;
         private System.Windows.Forms.Label IncorrectText;
+        private System.Windows.Forms.ListView Storage;
+        private System.Windows.Forms.ColumnHeader name;
+        private System.Windows.Forms.ColumnHeader price;
+        private System.Windows.Forms.ColumnHeader opslag;
+        private System.Windows.Forms.ColumnHeader size;
+        private System.Windows.Forms.ColumnHeader color;
+        private System.Windows.Forms.Button sellButton;
+        private System.Windows.Forms.Label paarText;
+        private System.Windows.Forms.NumericUpDown count;
     }
 }
 
--- Gimpies Winforms/Form1.cs
+++ Gimpies Winforms/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -13,12 +14,49 @@ namespace Gimpies_Winforms
 {
     public partial class Form1 : Form
     {
+        private System.Windows.Forms.Timer selectionTimer;
         public Form1()
         {
             InitializeComponent();
+            selectionTimer = new System.Windows.Forms.Timer();
+            selectionTimer.Interval = 1;
+            selectionTimer.Tick += SelectionTimer_Tick;
+            selectionTimer.Start();
+        }
+
+        public void SelectionTimer_Tick(object sender, EventArgs e)
+        {
+            if (Storage.SelectedItems.Count > 0 && count.Text.Length > 0)
+            {
+                sellButton.Enabled = true;
+            }
+            else
+            {
+                sellButton.Enabled = false;
+            }
+        }
+
+        public void EnableAll()
+        {
+            voorraadBekijkenToolStripMenuItem.Checked = false;
+            verkopenToolStripMenuItem.Checked = false;
+            toevoegenToolStripMenuItem.Checked = false;
+            aanpassenToolStripMenuItem.Checked = false;
+            verwijderenToolStripMenuItem.Checked = false;
+
+            voorraadBekijkenToolStripMenuItem.Enabled = true;
+            verkopenToolStripMenuItem.Enabled = true;
+            if (UsernameInput.Text == "m")
+            {
+                toevoegenToolStripMenuItem.Enabled = true;
+                aanpassenToolStripMenuItem.Enabled = true;
+                verwijderenToolStripMenuItem.Enabled = true;
+            }
         }
 
         int tries = 3;
+        string[] shoe = new string[20];
+        int[] countShoe = new int[20];
 
         private void LoginButton_Click(object sender, EventArgs e)
         {
@@ -35,6 +73,10 @@ namespace Gimpies_Winforms
                 Refresh();
                 voorraadBekijkenToolStripMenuItem.Enabled = true;
                 verkopenToolStripMenuItem.Enabled = true;
+                this.Text = "Menu";
+                countShoe[0] = 32;
+                countShoe[1] = 15;
+                countShoe[2] = 23;
             }
             else if (username == manager && password == passwordManager)
             {
@@ -45,6 +87,10 @@ namespace Gimpies_Winforms
                 toevoegenToolStripMenuItem.Enabled = true;
                 aanpassenToolStripMenuItem.Enabled = true;
                 verwijderenToolStripMenuItem.Enabled = true;
+                this.Text = "Menu";
+                countShoe[0] = 32;
+                countShoe[1] = 15;
+                countShoe[2] = 23;
             }
             else
             {
@@ -69,9 +115,66 @@ namespace Gimpies_Winforms
             }
         }
 
-        private void afsluitenToolStripMenuItem_Click(object sender, EventArgs e)
+        private void voorraadBekijkenToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            Application.Exit();
+            EnableAll();
+            voorraadBekijkenToolStripMenuItem.Checked = true;
+            voorraadBekijkenToolStripMenuItem.Enabled = false;
+        }
+
+        private void verkopenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            EnableAll();
+            verkopenToolStripMenuItem.Checked = true;
+            verkopenToolStripMenuItem.Enabled = false;
+        }
+
+        private void toevoegenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            EnableAll();
+            toevoegenToolStripMenuItem.Checked = true;
+            toevoegenToolStripMenuItem.Enabled = false;
+        }
+
+        private void aanpassenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            EnableAll();
+            aanpassenToolStripMenuItem.Checked = true;
+            aanpassenToolStripMenuItem.Enabled = false;
+        }
+
+        private void verwijderenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            EnableAll();
+            verwijderenToolStripMenuItem.Checked = true;
+            verwijderenToolStripMenuItem.Enabled = false;
+        }
+
+        private void voorraadBekijkenToolStripMenuItem_CheckedChanged(object sender, EventArgs e)
+        {
+            if (voorraadBekijkenToolStripMenuItem.Checked == true)
+            {
+                Storage.Visible = true;
+                Storage.Items[0].SubItems[2].Text = countShoe[0] + " paar";
+                Storage.Items[1].SubItems[2].Text = countShoe[1] + " paar";
+                Storage.Items[2].SubItems[2].Text = countShoe[2] + " paar";
+
+                count.Visible = true;
+                sellButton.Visible = true;
+                paarText.Visible = true;
+            }
+            else
+            {
+                Storage.Visible = false;
+                count.Visible = false;
+                sellButton.Visible = false;
+                paarText.Visible = false;
+            }
+        }
+
+        private void sellButton_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
